fdddb20de9b1f62fbbf523465fa6e335b8e4f5f8
Daria <aexxas@mail.ru>
2024-09-18 23:34:40 +0500

    Task2

---
+++ Tyuiu.DemchenkoAD.Sprint0.Task2.V0.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.DemchenkoAD.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.DemchenkoAD.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckMessageVaid()
+        {
+            var name = "Cаша";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Cаша", res);
+        }
+    }
+}
---
+++ Tyuiu.DemchenkoAD.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.DemchenkoAD.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.DemchenkoAD.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Cаша"));
+
+            Console.ReadKey();
+        }
+    }
+}
